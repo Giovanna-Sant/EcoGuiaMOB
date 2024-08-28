135f8f699bb8da54ec4447e09f60e1db16787f2f
Giovanna <giovanna.ssant@gmail.com>
2024-08-28 09:25:13 -0300

    biblioteca svg funcionando

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Logo from './app/assets/logo.svg'
 
 export default function App() {
   return (
     <View style={estilos.container}>
       <StatusBar style="auto" />
       <Text>Oi Dev!</Text>
+      <Logo />
     </View>
   );
 }
