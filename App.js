c64b8b95025b68c46e2eae5f566f463c319481d6
lucasmvaladao <contato.valadao14@gmail.com>
2024-09-02 10:07:24 -0300

    fix: adjusted all

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from '../EcoGuiaMOB/app/routes/AppNavigator'; // Certifique-se de que o caminho está correto
+ 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+}
+ 
