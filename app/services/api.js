e65dca261ad9448726d3490d75de78a194911d63
Erica Lelis <erica_lelis@outlook.com>
2024-10-24 21:08:50 -0300

    Arrumado problema de compatibilidade de expo CLI

--- app/services/api.js
+++ app/services/api.js
@@ -1,7 +1,8 @@
 import  Axios  from "axios";
 
 const api = Axios.create({
-    baseURL:'http://192.168.1.40:3000'
+    baseURL:'http://192.168.1.40:3000',
+    timeout: 5000
 });
 
 export default api;
